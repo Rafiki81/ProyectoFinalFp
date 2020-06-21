fda5a7917bf43c9c5d895180e65c808ff07617cb
rpere <rperezbeato@gmail.com>
2020-06-21 17:45:16 +0200

    Initial Commint.
    - Login Layout
    - Singup Layout
    
    Creación de los Ripples para los buttons.
    Creación del esquema general con colores y archivo dimen

---
+++ app/src/main/java/com/rafiki/chatapp/activities/SingUpActivity.kt
@@ -0,0 +1,12 @@
+package com.rafiki.chatapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rafiki.chatapp.R
+
+class SingUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+    }
+}
