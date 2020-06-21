2c0d4b290d51bccc85388515e337b30e94bd4fe2
rperez <rperezbeato@gmail.com>
2020-06-22 00:26:31 +0200

    Envío de correo de Verificación y checkeo si el email ha sido verificado.

--- app/src/main/java/com/rafiki/chatapp/activities/ForgotPaswordActivity.kt
+++ app/src/main/java/com/rafiki/chatapp/activities/ForgotPaswordActivity.kt
@@ -1,12 +1,44 @@
 package com.rafiki.chatapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.rafiki.chatapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.rafiki.chatapp.*
+import kotlinx.android.synthetic.main.activity_forgot_pasword.*
 
 class ForgotPaswordActivity : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance()}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_pasword)
+
+        editText_email.validate {
+            editText_email.error = if(isValidEmail(it)) null else "Email is not valid"
+        }
+
+        buttonGoLogIn.setOnClickListener {
+            goToActivity<LoginActivity>{
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
+
+        buttonForgot.setOnClickListener {
+            val email = editText_email.text.toString()
+            if(isValidEmail(email)){
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(this){
+                    toast("Email has been send to reset your password")
+                    goToActivity<LoginActivity>{
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    }
+                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+                }
+            }else{
+                toast("Please make sure the email adress is correct")
+            }
+        }
     }
 }
