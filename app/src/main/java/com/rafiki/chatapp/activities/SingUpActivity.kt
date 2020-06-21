f3e0466ff1716c1af293c3fc84f009fd3c2bf19f
rpere <rperezbeato@gmail.com>
2020-06-21 18:42:58 +0200

    Refactorización para dar funcionalidad a los botones de Sing IN.

--- app/src/main/java/com/rafiki/chatapp/activities/SingUpActivity.kt
+++ app/src/main/java/com/rafiki/chatapp/activities/SingUpActivity.kt
@@ -1,10 +1,12 @@
 package com.rafiki.chatapp.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.rafiki.chatapp.R
+import kotlinx.android.synthetic.main.activity_sing_up.*
 
 
 class SingUpActivity : AppCompatActivity() {
@@ -14,6 +16,27 @@ class SingUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sing_up)
+
+        buttonGoLogIn.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
+
+        buttonSingUp.setOnClickListener {
+
+            val email = editText_email.text.toString()
+            val password = editText_password.text.toString()
+
+            if(isValidEmailAndPassword(email,password)){
+                singUpByEmail(email,password)
+            }else{
+                Toast.makeText(this, "Please Fill all the data and confirm password is correct", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
 
         val currentUser = mAuth.currentUser
         if (currentUser == null) {
@@ -27,15 +50,21 @@ class SingUpActivity : AppCompatActivity() {
 
     }
 
-    private fun createAccount(email: String, password: String) {
+    private fun singUpByEmail(email: String, password: String) {
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    Toast.makeText(this, "CreateUserWithEmail:Succes", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "An Email has been Sent to You, please confirm before Sign In", Toast.LENGTH_LONG).show()
                     val user = mAuth.currentUser
                 } else {
-                    Toast.makeText(this, "CreateUserWithEmail:Failure", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "An Unexpected Error Occurred, please Try Again", Toast.LENGTH_LONG).show()
                 }
             }
+    }
+
+    private fun isValidEmailAndPassword(email: String, password: String): Boolean {
+
+        return !email.isNullOrEmpty() && !password.isNullOrEmpty() && password == editText_confirmPassword.text.toString()
+
     }
 }
